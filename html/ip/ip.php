780f8be8fb1dcad34cd2c3bd3b6b012d0cd5fb2b
adamjmilward@gmail.com <adamjmilward@gmail.com>
2019-10-28 23:18:24 +0000

    update ip script

--- html/ip/ip.php
+++ html/ip/ip.php
@@ -10,7 +10,9 @@ echo $newIP . '<br>';
 $oldIP = file_get_contents($myIPFile);
 echo $oldIP;
 
-if ($oldIP !== $newIP) {
+if (!$newIP) {
+  file_put_contents($lastUpdateFile, date('Y/m/d h:i:s', time()) . ' - no ip ' . "\n", FILE_APPEND);
+} elseif ($oldIP !== $newIP) {
     file_put_contents($myIPFile, $newIP);
     echo file_get_contents("https://adammilward.co.uk/thepi.php?new-ip=$newIP");
     file_put_contents($lastUpdateFile, date('Y/m/d h:i:s', time()) . ' - ' . $newIP . "\n", FILE_APPEND);
