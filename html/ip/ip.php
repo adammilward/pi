0ac2536e251c36f26c68d519dcf2467ab4107b5a
adamjmilward@gmail.com <adamjmilward@gmail.com>
2019-10-28 22:56:53 +0000

    update ip script

--- html/ip/ip.php
+++ html/ip/ip.php
@@ -1,15 +1,19 @@
 <?php
 
+$lastUpdateFile = '/var/www/html/ip/lastUpdate.txt';
+$myIPFile = '/var/www/html/ip/myIP.txt';
+
+
 $newIP = file_get_contents("http://ipecho.net/plain");
 echo $newIP . '<br>';
 
-$oldIP = file_get_contents("myip.txt");
+$oldIP = file_get_contents($myIPFile);
 echo $oldIP;
 
 if ($oldIP !== $newIP) {
-    file_put_contents('myip.txt', $newIP);
+    file_put_contents($myIPFile, $newIP);
     echo file_get_contents("https://adammilward.co.uk/thepi.php?new-ip=$newIP");
-    file_put_contents('lastUpdate.txt', date('Y/m/d h:i:s', time()) . ' - ' . $newIP . "\n", FILE_APPEND);
+    file_put_contents($lastUpdateFile, date('Y/m/d h:i:s', time()) . ' - ' . $newIP . "\n", FILE_APPEND);
 } else {
-  file_put_contents('lastUpdate.txt', date('Y/m/d h:i:s', time()) . ' - ' . getcwd() . "\n", FILE_APPEND);
+  file_put_contents($lastUpdateFile, date('Y/m/d h:i:s', time()) . ' - ' . getcwd() . "\n", FILE_APPEND);
 }
