bb9b4adf9d2bb40d2232c2017ee9e920ea72c999
adamjmilward@gmail.com <adamjmilward@gmail.com>
2020-01-19 21:37:34 +0000

    added build files

---
+++ html/React/build/precache-manifest.168e7906c7f667b1c955ad371b3651b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3447a92c883b9d77e8eafab4c665412",
+    "url": "/React/build/index.html"
+  },
+  {
+    "revision": "12e131e9998805a56375",
+    "url": "/React/build/static/js/2.bf2217a8.chunk.js"
+  },
+  {
+    "revision": "bfb19a4219c76289b554e6db627c1aec",
+    "url": "/React/build/static/js/2.bf2217a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e30479bd7f6de849a53",
+    "url": "/React/build/static/js/main.f5ec10b9.chunk.js"
+  },
+  {
+    "revision": "2e1e103230d8acfb5435",
+    "url": "/React/build/static/js/runtime-main.d549d412.js"
+  }
+]);
