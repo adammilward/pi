c788e6126816084e74043b025f3f51c4d67dbc3b
adamjmilward@gmail.com <adamjmilward@gmail.com>
2020-02-09 13:40:17 +0000

    testing serial coms

--- html/vanSerial.php
+++ html/vanSerial.php
@@ -2,6 +2,13 @@
 
 require_once ('serial/PhpSerial.php');
 
+?>
+<form action="" method="post">
+  <input type="text" name="message"/>
+</form>
+<?php
+
+
 $serial = new PhpSerial();
 
 //this is the port where my Arduino is. Check from the Arduino IDE to see yours!
@@ -16,46 +23,19 @@ $serial->confFlowControl("none");
 
 $serial->deviceOpen();
 
-// we will send the colors as csv strings
-$colors['red']   = '255, 0, 0';
-$colors['green'] = '0, 255, 0';
-$colors['blue']  = '0, 0, 255';
-
-/*foreach ($colors as $color => $value) {
-echo "Now sending $color\n";
-$serial->sendMessage($value . "\n");
-sleep(1);
-}*/
 
 echo "<pre>";
 $tStart = microtime(1);
 $s =  (isset($_GET['s'])) ? $_GET['s'] : 1000;
 $s *= 1000;
 
-//usleep($s);
-//$tStart = getTime($tStart, "sleep $s");
-//usleep($s);
 echo htmlentities($serial->readPort() . "\n");
-//$tStart = getTime($tStart, 'read 1');
 
-//usleep($s);
-//$tStart = getTime($tStart, "sleep $s");
-//$serial->sendMessage((string) 'hello');
-foreach (str_split($_GET['msg']) as $letter) {
-	//echo ($letter);
-	$serial->sendMessage($letter,  0.003);
+foreach (str_split($_post['message']) as $letter) {
+  $serial->sendMessage($letter,  0.003);
 }
-$serial->sendMessage("\n" . $_GET['msg'] . "\n", 2);
-$serial->sendMessage("012345678901234567890\n");
-//$serial->sendMessage((string) $_GET['msg'] . "     \n", 1);
-//$tStart = getTime($tStart, 'write');
-
-//usleep($s);
-//$tStart = getTime($tStart, "sleep $s");
 
 echo htmlentities($serial->readPort() . "\n");
-//$tStart = getTime($tStart, 'read 2');
-
 
 echo "\nDONE\n";
 echo "</ pre>";
