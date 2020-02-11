56d511b593ed01de0ebe3c14861732d19609ebf6
adamjmilward@gmail.com <adamjmilward@gmail.com>
2020-02-11 21:10:54 +0000

    use python script because php serial is a bit rubish

--- html/sendReceiveSerial.py
+++ html/sendReceiveSerial.py
@@ -2,26 +2,16 @@
 
 import serial
 import time
-start = int(round(time.time() * 1000))
-print ("start " + str(start - int(round(time.time() * 1000))))
-
-ser = serial.Serial('/dev/serial0', 57600, timeout = 0.1)
-print ("serial initialised " + str(start - int(round(time.time() * 1000))))
-
-ser.write("lights ru\n".encode())
-print ("written " + str(start - int(round(time.time() * 1000))))
 
 output = ""
+
+ser = serial.Serial('/dev/ttyACM0', 57600, timeout = 0.1)
+ser.write("lights ru\n".encode())
+
 response = ser.readlines()
-print ("got response " + str(start - int(round(time.time() * 1000))))
 for line in response:
     output += line.decode('utf-8')
     print(line.decode('utf-8'))
-    #response = ser.readline()
-    print ("got response next " + str(start - int(round(time.time() * 1000))))
-    
-    
-ser.close()    
-print("exit " + str(start - int(round(time.time() * 1000))))
-exit(output)
 
+ser.close()
+exit(output)
