ee0d09a8e4f2688fe328a7caded5ac327c873f6a
adamjmilward@gmail.com <adamjmilward@gmail.com>
2020-10-29 16:22:29 +0000

    extend the ip update script to change ap server and client modes

--- html/ip/ip.php
+++ html/ip/ip.php
@@ -2,24 +2,61 @@
 
 require_once '/var/www/html/config.php';
 
-$lastUpdateFile = '/var/www/html/ip/lastUpdate.txt';
-$myIPFile = '/var/www/html/ip/myIP.txt';
+$webRoute = $config['web_route'] ?? '/var/www/html';
+$lastUpdateFile = $webRoute . '/ip/lastUpdate.txt';
+$myIPFile = $webRoute . '/ip/myIP.txt';
 
-
-$newIP = file_get_contents("http://ipecho.net/plain");
+//$newIP = file_get_contents("http://ipecho.net/plain");
+$newIP = false;
 echo $newIP . '<br>';
 
-$oldIP = file_get_contents($myIPFile);
-echo $oldIP;
+$wifiMode = file_get_contents($webRoute . '/ip/mode.txt');
+if ($wifiMode === 'ap') {
+  $time = new \DateTimeImmutable('NOW');
+  // between 3 and 5 past we shut down and attempt wifi mode
+  // it will take more than 5 minutes to switch from wifi back to ap if we do not detect an ip
+  if ((int)$time->format('H') === 3 && (int)$time->format('i') < 5) {
+    shell_exec($webRoute . '/ip/wifi.sh');
+  }
+  // no point in continuing if we are an access point
+  exit();
+}
 
+$ipData = json_decode(file_get_contents($myIPFile), true);
+if (! $ipData) {
+  $ipData = [
+    'ip' => '-',
+    'failed_attempts' => 0
+  ];
+}
+$oldIP = $ipData['ip'];
+
+// get the args that the script was called with
 $arg = (isset($argc) && isset($argv[1])) ? $argv[1] : '';
 
-if (!$newIP) {
+
+if (! $newIP) {
+    $ipData['failed_attempts'] += 1;
     file_put_contents($lastUpdateFile, date('Y/m/d h:i:s', time()) . " | arg: $arg - no ip \n", FILE_APPEND);
-} elseif ($oldIP !== $newIP) {
-    file_put_contents($myIPFile, $newIP);
+} else {
+
+  $ipData['failed_attempts'] = 0;
+  $ipData['ip'] = $newIP;
+
+  if ($oldIP !== $newIP) {
     echo file_get_contents($config['remoteAddress'] . "?new-ip=$newIP");
     file_put_contents($lastUpdateFile, date('Y/m/d h:i:s', time()) . " | arg  $arg - newIP: $newIP \n", FILE_APPEND);
-} else {
+  } else {
     file_put_contents($lastUpdateFile, date('Y/m/d h:i:s', time()) . " | arg: $arg - no change \n", FILE_APPEND);
+  }
 }
+
+// if we have 5 failed attempts in a row (5 minutes) then switch to access point mode
+if ($ipData['failed_attempts'] > 5 && $wifiMode === 'wifi') {
+  $ipData['failed_attempts'] = 0;
+  file_put_contents($myIPFile, json_encode($ipData));
+  shell_exec($webRoute . '/ip/ap.sh');
+  exit(); // unnecessary as the above command will reboot the computer
+}
+
+file_put_contents($myIPFile, json_encode($ipData));
