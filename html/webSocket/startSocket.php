a6b24d3f693e856fcc69f28fbe2f96f2a0b2aeda
adamjmilward@gmail.com <adamjmilward@gmail.com>
2020-12-27 18:07:55 +0000

    debugin

--- html/webSocket/startSocket.php
+++ html/webSocket/startSocket.php
@@ -4,8 +4,8 @@
  * User: adam
  * Date: 16/12/2020
  */
-$configs = json_decode(file_get_contents('config.json'));
-
+$configs = json_decode(file_get_contents('/var/www/html/config.json'));
+print_r($configs);
 header("Access-Control-Allow-Origin: http://localhost:3000");
 
 $response = shell_exec ($configs['checkServer.py']);
